23ab5e7f8f62aba7e229b5d47fa247d1c83c891b
sashafam <sashafam65@gmail.com>
2023-08-09 00:40:35 +0700

    kk

---
+++ run_all.js
@@ -0,0 +1,13 @@
+// Подключение файлов
+const fitplus = require('./fitplus.js');
+const fitplus_tg = require('./fitplus_tg.js');
+const mercado = require('./mercado.js');
+const mercado_tg = require('./mercado_tg.js');
+const miproteina = require('./miproteina.js');
+const miproteina_tg = require('./miproteina_tg.js');
+const nutricore = require('./nutricore.js');
+const nutricore_tg = require('./nutricore_tg.js');
+const suplementoscolombia = require('./suplementoscolombia.js');
+const suplementoscolombia_tg = require('./suplementoscolombia_tg.js');
+const zonafit = require('./zonafit.js');
+const zonafit_tg = require('./zonafit_tg.js');
